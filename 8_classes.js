8f2aac5f879e5f67329030541aabc75f36753e50
alex baboy <alexbaboy2016@gmail.com>
2021-08-23 15:56:05 +0300

    classes

--- 8_classes.js
+++ 8_classes.js
@@ -9,8 +9,40 @@ class Person {
         console.log(this.name + ' says Hello!')
     }
 }
+class Programmer extends Person {
+    constructor(name, job) {
+        super(name);
+        this._job = job
+    }
 
-const max = new Person('Max')
-console.log(max)
-max.greet()
-console.log(max.type)
+    get job() {
+        return this._job.toUpperCase()
+    }
+
+    set job(job) {
+        if( job.length < 2 ) {
+            throw new Error('validation failed!')
+        }
+        this._job = job
+    }
+
+    greet() {
+        super.greet()
+        console.log('rewritten')
+    }
+}
+
+const frontend = new Programmer('Max', 'Frontend')
+/*
+console.log(frontend.job)
+//frontend.job = 'backend'
+frontend.job = 'b'
+console.log(frontend.job)*/
+
+class Util {
+    static average(...args) {
+        return args.reduce((acc, i) => acc += i, 0) / args.length
+    }
+}
+
+console.log(Util.average(1,1,2,3,5,8,13))
